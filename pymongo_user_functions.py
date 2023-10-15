7e47a41ca7f06f6ed204b87928dba83bd6b6e8f3
Stephan Tserovski <penguinman299@gmail.com>
2023-10-14 20:51:12 -0400

    Added Mongo functions for student users
    Can now:
    - Get a Student's lectures from the database
    - See if a Student with the specified name exists
    - Add new Students
    - Add a lecture to an existing Student.

---
+++ pymongo_user_functions.py
@@ -0,0 +1,51 @@
+from pymongo import MongoClient
+def get_database():
+ 
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   CONNECTION_STRING = "mongodb+srv://Stephan:68noDnGIBUnAjrao@firefly.mkpxt60.mongodb.net/"
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create the database for our example (we will use the same database throughout the tutorial
+   return client['Users']
+  
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":   
+  
+   # Get the database
+   dbname = get_database()
+
+def get_student_lectures(name):
+   student = get_database()["Students"].find({"name" : name})[0]
+   return student["lectures"]
+   
+def student_exists(name):
+   try:
+      student = get_database()["Students"].find({"name" : name})[0]
+   except IndexError:
+      return False
+   return True
+
+def add_student_lecture(name, lecture):
+   cname = get_database()["Students"]
+   updateResult = cname.update_one( 
+      { "name": name },
+      { "$addToSet": { "lectures": lecture } }
+   )
+   if updateResult.matched_count == 0:
+      new_student = {
+         "name" : name,
+         "lectures" : [lecture]
+      }
+      cname.insert_one(new_student)
+   
+
+def get_professor_lectures(name):
+   return name
+   
+def professor_exists(name):
+   return name
+
+def add_professor_lecture(name, lecture):
+   return
